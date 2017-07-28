0ee5e329efc05e60557bce8f308da94a810b0549
Lu <lucrecia.villegas@dinocloudconsulting.com>
2017-07-28 11:35:47 -0300

    authorization method modified

--- source/utils.py
+++ source/utils.py
@@ -2,13 +2,14 @@ from basicauth import decode
 from models import *
 from werkzeug.exceptions import BadRequest, NotFound
 
+
 def authorization(encoded_str):
-    if encoded_str == None:
+    if encoded_str is None:
         raise BadRequest('Authorization failed')
     username, password = decode(encoded_str)
     user = User.query.filter_by(username=username).first()
-    if user == None:
+    if user is None:
         raise NotFound('User not found')
-    elif user.password == password:
-        return user
-    raise NotFound('Incorrect password')
+    elif user.password is not password:
+        raise NotFound('Incorrect password')
+    return user
