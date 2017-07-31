d93b4ecbd99fb5a9ce40dccf8691acca223d827d
Lu <lucrecia.villegas@dinocloudconsulting.com>
2017-07-31 16:54:52 -0300

    pagination for tasks api implemented

--- source/views/tasks_view.py
+++ source/views/tasks_view.py
@@ -4,22 +4,33 @@ from models import *
 from schemas import *
 # from sqlalchemy_pagination import paginate
 
+from utils import *
+
 
 class TasksView(FlaskView):
     task_schema = TaskSchema()
 
+    class TasksPaginationSchema(PaginationSchema):
+        items = fields.List(fields.Nested(TaskSchema))
+
+    pagination_schema = TasksPaginationSchema()
+
     def index(self):
-        tasks = Task.query.all()
-        # page = Task.query.order_by(Task.paginate(1,10,False))
-        tasks_data = self.task_schema.dump(tasks, many=True).data
-        return jsonify({'task': tasks_data}), 200
+        user = authorization(request.headers.get('Authorization', None))
+        page = request.args.get('Page', None)
+        per_page = request.args.get('Per_page', None)
+        tasks = Task.query.paginate(page, per_page, error_out=False)
+        tasks_data = self.pagination_schema.dump(tasks).data
+        return jsonify(tasks_data), 200
 
     def get(self, id_task):
+        user = authorization(request.headers.get('Authorization', None))
         task = Task.query.filter_by(id_task=int(id_task)).first()
         task_data = self.task_schema.dump(task).data
         return jsonify({'task': task_data})
 
     def post(self):
+        user = authorization(request.headers.get('Authorization', None))
         data = request.json
         task_name = data.get('task_name', None)
         if not task_name:
@@ -38,6 +49,7 @@ class TasksView(FlaskView):
 
 
     def put(self, id_task):
+        user = authorization(request.headers.get('Authorization', None))
         data = request.json
         task = Task.query.filter_by(id_task=int(id_task)).first()
         task.task_name = data.get('task_name', None)
@@ -53,6 +65,7 @@ class TasksView(FlaskView):
 
 
     def delete(self, id_task):
+        user = authorization(request.headers.get('Authorization', None))
         try:
             db.session.delete(Task.query.get(id_task))
             db.session.commit()
